a78aea24074dd492a496258f2d3c8be8d39a633e
hyemyu03 <hyemyu03@gmail.com>
2025-10-05 14:15:46 +0900

    Add files via upload
    Signed-off-by: hyemyu03 <hyemyu03@gmail.com>

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='fortune-v1';
+const ASSETS=['/','/index.html','/manifest.json','/assets/icon-192.png','/assets/icon-512.png','/assets/og-image.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return res;
+    }).catch(()=>caches.match('/index.html')))
+  );
+});
